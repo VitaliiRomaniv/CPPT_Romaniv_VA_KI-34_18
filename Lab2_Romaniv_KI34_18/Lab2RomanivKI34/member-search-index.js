c1c249fc413909991ca5f600a94a52961a1bfbb7
Vitalii Romaniv <vitalii.romaniv03@gmail.com>
2022-09-02 15:57:12 +0300

    Lab2RomanivKI34

---
+++ Lab2_Romaniv_KI34_18/Lab2RomanivKI34/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab2RomanivKI34","l":"Lab2RomanivKI34()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab2RomanivKI34","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
